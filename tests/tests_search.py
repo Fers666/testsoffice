352291580169b53d83cad205b317ea10aa7f8bdc
Fers666 <59649804+Fers666@users.noreply.github.com>
2023-03-29 17:26:19 +0300

    testing PageObject 1

--- tests/tests_search.py
+++ tests/tests_search.py
@@ -1,14 +1,17 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 from pages.search_page import SearchPage
+import datetime
 
+async def main():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
 
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-
-    page = browser.new_page()
-    search_page = SearchPage(page)
-    search_page.navigate()
-    search_page.search("search query")
-    page.screenshot(path="screenshots\\example.png")
-    print(page.title())
-    browser.close()
+        page = await browser.new_page()
+        search_page = SearchPage(page)
+        await search_page.navigate()
+        await search_page.search("search query")
+        await page.screenshot(path=f"screenshots\\example.png")
+        print(await page.title())
+        await browser.close()
+asyncio.run(main())
