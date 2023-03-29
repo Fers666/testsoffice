9cf7364cf5ac9706063973faf1cc15ff2e416991
Evgeni Egorov <59649804+Fers666@users.noreply.github.com>
2023-03-29 17:51:59 +0300

    upper models

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,11 +1,13 @@
+import asyncio
+import pytest
 class SearchPage:
     def __init__(self, page):
         self.page = page
         self.search_term_input = page.locator('[aria-label="Enter your search term"]')
 
-    def navigate(self):
-        self.page.goto("https://bing.com")
+    async def navigate(self):
+        await self.page.goto("https://bing.com")
 
-    def search(self, text):
-        self.search_term_input.fill(text)
-        self.search_term_input.press("Enter")
+    async def search(self, text):
+        await self.search_term_input.fill(text)
+        await self.search_term_input.press("Enter")
--- tests/tests_search.py
+++ tests/tests_search.py
@@ -1,9 +1,11 @@
 import asyncio
+
+import pytest
 from playwright.async_api import async_playwright
 from pages.search_page import SearchPage
-import datetime
 
-async def main():
+@pytest.mark.asyncio
+async def test_input():
     async with async_playwright() as playwright:
         browser = await playwright.chromium.launch(headless=False)
 
@@ -14,4 +16,4 @@ async def main():
         await page.screenshot(path=f"screenshots\\example.png")
         print(await page.title())
         await browser.close()
-asyncio.run(main())
+# asyncio.run(test_input())
